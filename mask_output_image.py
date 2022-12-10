56d3e5fa1a6d79b22bb2c6f227e21cf94860ba1e
baselhindi <basel.hindi.95@gmail.com>
2022-12-10 00:52:52 +0000

    commit 4

--- mask_output_image.py
+++ mask_output_image.py
@@ -1,51 +1,30 @@
-# import the necessary packages
 import numpy as np
 import cv2
 import os
 
 
-# Displaying the contents of the text file
-# file = open("bboxes_for_mask.txt", "r")
-# # content = file.read()
-
-# Lines = file.readlines()
-  
-# count = 0
-# # Strips the newline character
-# for line in Lines:
-#     count += 1
-#     print("Line{}: {}".format(count, line.strip()))
-
-# file.close()
-
-# i = 0
+## get the total number of output images from the detection model
 folder = "output_images/"
 no_of_files = len(os.listdir(folder))
-print("no of files: ", no_of_files)
+# print("no of files: ", no_of_files)
 
-# for filename in os.listdir(folder):
+## get filename for each unmasked file, ensuring alignment between bounding boxes and unmasked images 
 for i in range(no_of_files):
-    # print(filename)
-    filename = str(i) + "_unmasked.jpg"
-        
+    filename = str(i) + "_unmasked.jpg"  
     clean_bbox_file = "clean_bboxes_folder/" + str(i) + "_clean_bboxes_for_mask.txt"
-    # File_data = np.loadtxt("clean_bboxes_for_mask.txt", dtype=float)
     File_data = np.loadtxt(clean_bbox_file, dtype=float)
-
-    image = cv2.imread(folder+filename)
-    # image = cv2.imread("result1.jpg")
-                                                             
+    image = cv2.imread(folder+filename)    
+    ## create mask 
     mask = np.zeros(image.shape[:2], dtype="uint8")
 
+    ## use bounding box dimensions to create rectangles that will remain unmasked
     for j in range(len(File_data)):
         cv2.rectangle(mask, (int(File_data[j][0]), int(File_data[j][1])), (int(File_data[j][2]), int(File_data[j][3])), 255, -1)
 
+    ## apply the mask
     masked = cv2.bitwise_and(image, image, mask=mask)
     
+    ## create output file, now masked
     output_filename = "masked_outputs/" + str(i) + "_masked_frame.jpg"
-    print(output_filename)
     cv2.imwrite(output_filename, masked)
-    # cv2.imwrite("result_mask2.jpg", masked)
-    # cv2.imshow("Mask Applied to Image", masked)
-    # i += 1
     cv2.waitKey(0)
